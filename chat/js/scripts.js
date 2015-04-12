0b5e15dfdbefa1159edda0e6219139b35d2193b1
slawiko <shchaurouski.slava@gmail.com>
2015-04-12 10:11:21 +0300

    get post put delete added

--- chat/js/scripts.js
+++ chat/js/scripts.js
@@ -16,6 +16,12 @@ var messageStruct = function (text, user) {
 
 var messageList = [];
 
+var appState = {
+	mainUrl : 'http://localhost:1555/todos',
+	messageList: [],
+	token : 'TE11EN'
+};
+
 function updateMessageList(newMessage, messageListI) {
     messageListI.textMessage = newMessage;
 }
@@ -291,4 +297,73 @@ function addMessage(message) {
     newMessage.id = message.id;
 	messages.appendChild(newMessage);
     messageList.push(message);
+}
+
+function isError(text) {
+	if(text == "")
+		return false;
+	
+	try {
+		var obj = JSON.parse(text);
+	} catch(ex) {
+		return true;
+	}
+
+	return !!obj.error;
+}
+
+function Get(url, continueWith, continueWithError) {
+	ajax('GET', url, null, continueWith, continueWithError);
+}
+
+function Post(url, data, continueWith, continueWithError) {
+	ajax('POST', url, data, continueWith, continueWithError);	
+}
+
+function Put(url, data, continueWith, continueWithError) {
+	ajax('PUT', url, data, continueWith, continueWithError);	
+}
+
+function Delete(url, data, continueWith, continueWithError) {
+	ajax('DELETE', url, data, continueWith, continueWithError);	
+}
+
+function ajax(method, url, data, continueWith, continueWithError) {
+	var xhr = new XMLHttpRequest();
+
+	continueWithError = continueWithError || defaultErrorHandler;
+	xhr.open(method || 'GET', url, true);
+
+	xhr.onload = function () {
+		if (xhr.readyState !== 4)
+			return;
+
+		if(xhr.status != 200) {
+			continueWithError('Error on the server side, response ' + xhr.status);
+			return;
+		}
+
+		if(isError(xhr.responseText)) {
+			continueWithError('Error on the server side, response ' + xhr.responseText);
+			return;
+		}
+
+		continueWith(xhr.responseText);
+	};    
+
+    xhr.ontimeout = function () {
+    	ontinueWithError('Server timed out !');
+    }
+
+    xhr.onerror = function (e) {
+    	var errMsg = 'Server connection error !\n'+
+    	'\n' +
+    	'Check if \n'+
+    	'- server is active\n'+
+    	'- server sends header "Access-Control-Allow-Origin:*"';
+
+        continueWithError(errMsg);
+    };
+
+    xhr.send(data);
 }
